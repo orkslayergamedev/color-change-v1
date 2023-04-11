741f21f26378100ca7f71c4a8b8ee1a501ecf912
Ork Slayer Gamedev <orkslayergamedev@gmail.com>
2023-04-11 09:30:32 +0200

    Basic Window with comments

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+# imports
 import pygame
 
-W, H = 800, 600
+# initializing pygame
+pygame.init()
 
+# variables
+W, H = 800, 600
+run = True
+
+# window setup
 display = pygame.Surface((W, H))
 screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption("colored_text")
 clock = pygame.time.Clock()
 
-pygame.init()
-
-run = True
-
+# main loop
 while run:
+    # events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    # screen update
     clock.tick()
-
     display.blit(screen, (0, 0))
     pygame.display.update()
